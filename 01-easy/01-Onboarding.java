430f39ecede08e0432415e2d6415c3713c1d140c
Edin Fazlic <edin.fazlic@gmail.com>
2015-06-27 17:35:40 +0200

    Create 01-Onboarding.java

---
+++ 01-easy/01-Onboarding.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * CodinGame planet is being attacked by slimy insectoid aliens.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            String enemy1 = in.next(); // name of enemy 1
+            int dist1 = in.nextInt(); // distance to enemy 1
+            String enemy2 = in.next(); // name of enemy 2
+            int dist2 = in.nextInt(); // distance to enemy 2
+
+            System.out.println(dist1 < dist2 ? enemy1 : enemy2);
+        }
+    }
+}
